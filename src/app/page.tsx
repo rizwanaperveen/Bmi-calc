612ece1d578ebcc99d990c75c85cbce2fdb4e71c
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-10-13 07:51:31 +0500

    Add files via upload
    BMI-calculator

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BmiCalculator from '@/components/bmi'
+const Home = () => {
+  return (
+    <div>
+      <BmiCalculator />
+    </div>
+  )
+}
+
+export default Home
